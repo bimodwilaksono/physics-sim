6a5f7a2759582cb8dc75a4c8902bfd200e11e20c
Bimo Laksono <bimodwilaksono@gmail.com>
2026-01-24 19:42:08 +0700

    fix: overflow scroll issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,7 +13,7 @@ export const Route = createFileRoute("/")({ component: App });
 
 function App() {
 	return (
-		<div className="min-h-screen min-w-screen flex items-center justify-center  p-4">
+		<div className="min-h-screen min-w-screen flex items-center justify-center p-4 overscroll-none">
 			<Card className="w-full max-w-2xl shrink-0 bg-transparent border-none shadow-none">
 				<CardHeader className="text-center pb-2">
 					<CardTitle className="text-4xl font-bold text-white pixel-font">
@@ -26,7 +26,11 @@ function App() {
 				</CardHeader>
 				<CardContent className="flex flex-col gap-4 pt-2">
 					<Link to="/simulator/momentum" className="w-full">
-						<Button variant="default" size="lg" className="w-full pixel-font hover:-translate-y-2">
+						<Button
+							variant="default"
+							size="lg"
+							className="w-full pixel-font hover:-translate-y-2"
+						>
 							Momentum Simulator
 						</Button>
 					</Link>
